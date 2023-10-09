75ee38c9ff405af383ae56b11d065788e3e45665
Nathan Prisbrey <ntpbanana@gmail.com>
2023-10-09 07:45:10 +0000

    Change manual_SDPA To Function

--- manual_transformer.py
+++ manual_transformer.py
@@ -5,26 +5,18 @@ from torch import nn
 D_MODEL = 512
 
 
-class manual_SDPA(nn.Module):
-    def __init__(self):
-        """
-        Args:
-
-        """
-        super().__init__()
-
-    def forward(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor) -> torch.Tensor:
-        """
-        Args:
-            q (tensor): The queries, of dimension (batch_size, d_k, D_MODEL).
-            k (tensor): The keys, of dimension (batch_size, d_k, D_MODEL).
-            v (tensor): The values, of dimension (batch_size, d_v, D_MODEL).
-        """
-        q_mul_k = torch.bmm(q, k.transpose(-2, -1))
-        scaled = torch.div(q_mul_k, sqrt(k.shape[-1]))
-        e_powered = torch.exp(scaled)
-        softmaxed = torch.div(e_powered, torch.sum(e_powered).unsqueeze(-1))
-        return torch.bmm(softmaxed, v)
+def manual_SDPA(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor) -> torch.Tensor:
+    """
+    Args:
+        q (tensor): The queries, of dimension (*, d_k, D_MODEL).
+        k (tensor): The keys, of dimension (*, d_k, D_MODEL).
+        v (tensor): The values, of dimension (*, d_v, D_MODEL).
+    """
+    q_mul_k = torch.matmul(q, k.transpose(-2, -1))
+    scaled = torch.div(q_mul_k, sqrt(k.shape[-1]))
+    e_powered = torch.exp(scaled)
+    softmaxed = torch.div(e_powered, torch.sum(e_powered).unsqueeze(-1))
+    return torch.matmul(softmaxed, v)
 
 
 class manual_MHA(nn.Module):
