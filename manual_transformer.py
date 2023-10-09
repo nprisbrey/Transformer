b6ea9506239837eba92b4d566d5cf1e66ecac04f
Nathan Prisbrey <ntpbanana@gmail.com>
2023-10-09 07:45:32 +0000

    Fill Out manual_MHA

--- manual_transformer.py
+++ manual_transformer.py
@@ -26,27 +26,50 @@ class manual_MHA(nn.Module):
             num_heads (int): Number of heads to concatenate together at end.
         """
         super().__init__()
-        self.v_lin_projs = (nn.Linear(in_features=D_MODEL,
-                                      out_features=D_MODEL/num_heads)
-                            for head in range(num_heads))
-        self.k_lin_projs = (nn.Linear(in_features=D_MODEL,
-                                      out_features=D_MODEL/num_heads)
-                            for head in range(num_heads))
-        self.q_lin_projs = (nn.Linear(in_features=D_MODEL,
-                                      out_features=D_MODEL/num_heads)
-                            for head in range(num_heads))
+        self.num_heads = num_heads
+        assert D_MODEL % num_heads == 0, \
+            f"D_MODEL / num_heads must divide evenly, not {D_MODEL} / {num_heads}!"
+        self.d_k = int(D_MODEL / num_heads)
+        self.d_v = int(D_MODEL / num_heads)
+        self.q_lin_projs = nn.Parameter(torch.randn(num_heads,
+                                                    D_MODEL,
+                                                    self.d_k))
+        self.k_lin_projs = nn.Parameter(torch.randn(num_heads,
+                                                    D_MODEL,
+                                                    self.d_k))
+        self.v_lin_projs = nn.Parameter(torch.randn(num_heads,
+                                                    D_MODEL,
+                                                    self.d_v))
+        self.concat_lin_proj = nn.Parameter(torch.randn(num_heads * self.d_v,
+                                                        D_MODEL))
 
-
-    def forward(self, v: torch.Tensor, k: torch.Tensor, q: torch.Tensor) -> torch.Tensor:
+    def forward(self, embeded_seq: torch.Tensor) -> torch.Tensor:
         """
         Args:
-            v (tensor): The values, of dimension (batch_size, d_v, D_MODEL).
-            k (tensor): The keys, of dimension (batch_size, d_k, D_MODEL).
-            q (tensor): The queries, of dimension (batch_size, d_k, D_MODEL).
+            embeded_seq (tensor): A matrix of the embeded sequence, of
+                dimension (seq_len, D_MODEL).
         """
-        #TODO: For each v, k, q in batches, run v, k, q through all lin_projs
+        # Create matrix with a copy of the input sequence for each head
+        expanded_seq = embeded_seq.expand(self.num_heads, -1, -1)
 
-        pass
+        # Results have dimensions of (num_heads, seq_len, d_k or d_v)
+        qs = torch.matmul(expanded_seq, self.q_lin_projs)
+        ks = torch.matmul(expanded_seq, self.k_lin_projs)
+        vs = torch.matmul(expanded_seq, self.v_lin_projs)
+
+        print(f"qs: {qs.shape}")
+        print(f"ks: {ks.shape}")
+        print(f"vs: {vs.shape}")
+
+        # Calculate attentions
+        head_attns = manual_SDPA(qs, ks, vs)
+
+        print(f"head_attns: {head_attns.shape}")
+
+        # Concat head outputs to dimensions of (seq_len, self.num_heads * self.d_v)
+        concat_heads = torch.cat([head_attn.squeeze(dim=0) for head_attn in torch.split(head_attns, 1, dim=0)], dim=1)
+
+        return torch.matmul(concat_heads, self.concat_lin_proj)
 
 class manual_encoder(nn.Module):
     def __init__(self, d_model: int):
